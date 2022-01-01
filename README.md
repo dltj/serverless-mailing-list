555e13f0aa067bf85fb7c7303a758c549e18d742
Peter Murray <jester@dltj.org>
2021-12-31 22:47:35 -0500

    Complete initial serverless framework

---
+++ README.md
@@ -0,0 +1,5 @@
+## Set Up Environment
+1. `cd serverless-mailing-list`
+1. `pipenv install --dev`
+1. `pipenv run nodeenv -p`
+1. `pipenv shell` 
--- serverless/handler.py
+++ serverless/handler.py
@@ -1,7 +1,72 @@
 import json
 
 
-def hello(event, context):
+def subscribe(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+def confirm(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+def unsubscribe(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+def create_issue(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+def send_issue(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+def dynamodb_backup(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
